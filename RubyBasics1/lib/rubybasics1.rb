65e23e3ccb003d6ecc023df9199a896c3aef8335
Seamus Beecher <sbeeche1@uncc.com>
2019-09-03 18:28:07 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -2,12 +2,15 @@
 # Part I
 def sum arr
   # YOUR CODE HERE
+  
   return arr.sum
+  
 end
 
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  
   if arr.empty? == true
     return 0
   elsif arr.length == 1
@@ -15,9 +18,31 @@ def max_2_sum arr
   else
     return arr.sort.last(2).sum
   end
+  
 end
 
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  
+  #OR1
+  return false if arr.empty? && n.zero?
+  arr.combination(2).any? {|a, b| a + b == n }
+  
+  #OR2
+#  return false if arr.empty? && n.zero?
+#  arr.combination(2).any? {|a, b| a + b == n }
+  
+  #OR3
+#  for i in 0..arr.length - 1
+#    k = i + 1
+#    for k in k..arr.length - 1
+#      sum = arr[i] + arr[k]
+#      if sum == n
+#        return true
+#      end
+#    end
+#  end
+#  return false if n == 0 && arr.length == 0
+
 end
