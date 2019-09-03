34f0f62c9f2dcdc1a385e8225ca4055eb0b161d8
Seamus Beecher <sbeeche1@uncc.com>
2019-09-03 14:37:31 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,17 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  return arr.sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+  
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
