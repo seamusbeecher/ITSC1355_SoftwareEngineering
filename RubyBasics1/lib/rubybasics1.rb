8e64dc23beaa8fc32aafd94822a0a250a69757b1
Seamus Beecher <sbeeche1@uncc.com>
2019-09-03 15:05:31 +0000

    RubyBasics1 - Part 2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -8,7 +8,13 @@ end
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
-  
+  if arr.empty? == true
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    return arr.sort.last(2).sum
+  end
 end
 
 # Part III
