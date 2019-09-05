5cac3bf0ffaa61be31c27d2f18bb0dd6f84c9681
Seamus Beecher <sbeeche1@uncc.com>
2019-09-05 18:50:18 +0000

    RubyHashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,6 +2,20 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    temp = {}
+    i = 0
+    
+    if emails[0] == nil
+        return contacts
+    else
+        contacts.each do |key, value|
+            tempK = "#{key}"
+            temp[tempK] = emails[i]
+            i += 1
+        end
+    end
+    return temp
+    
 end
 
 # Part II
