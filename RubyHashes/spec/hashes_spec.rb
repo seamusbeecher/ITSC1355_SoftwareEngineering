47f7e1dc33807061ee0014a32b28638354635f63
Seamus Beecher <sbeeche1@uncc.com>
2019-09-15 20:28:38 +0000

    RubyBasics - Part 3 rspec enabled

--- RubyHashes/spec/hashes_spec.rb
+++ RubyHashes/spec/hashes_spec.rb
@@ -37,7 +37,7 @@ describe 'Ruby Hashes Part II' do
     
     it "works on the empty array [10 points]" , points: 10 do
       expect { array2d_2_hash([[]], {'Bob Smith'=>{}, 'Sally Field'=>{}}) }.not_to raise_error
-      expect(array2d_2_hash([[]], {'Bob Smith'=>{}, 'Sally Field'=>{}})).to eq({:"Bob Smith"=>{}, :"Sally Field"=>{}})
+      expect(array2d_2_hash([[]], {'Bob Smith'=>{}, 'Sally Field'=>{}})).to eq({"Bob Smith"=>{}, "Sally Field"=>{}})
     end
   end
 end
