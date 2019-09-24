3a0937d3fb8e770b1fdee6121a32c580b3f0d3de
Seamus Beecher <sbeeche1@uncc.com>
2019-09-24 14:24:59 +0000

    Ruby 00 Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -2,4 +2,12 @@
 
 class BookInStock
 # YOUR CODE HERE
+    attr_accessor :isbn
+    attr_accessor :price
+    def initialize(isbn, price)
+    # your code here
+        @isbn = isbn
+        @price = price
+    end
+
 end
